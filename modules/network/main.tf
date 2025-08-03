bf5710ff961a3feffe38c036e09175072b383c18
meleksabit <angel3.85bg@gmail.com>
2025-08-03 20:56:45 +0300

    edit security groups

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -73,8 +73,8 @@ resource "aws_security_group" "allow_http" {
   vpc_id      = aws_vpc.this.id
 
   ingress {
-    from_port   = 3000
-    to_port     = 3000
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] # Recommendation: Restrict in prod
   }
