829d7963a5ec6b246572046459e73cc943f190d7
𝓜𝓮𝓵𝓮𝓴 𝓢𝓪𝓫𝓲𝓽 <32045473+meleksabit@users.noreply.github.com>
2025-08-04 19:43:03 +0300

    chore: add infra + README (#3) (#4) (#5)
    * feat(infra): add terraform infra (#1)
    
    * add terraform infra files
    
    * edit infra
    
    * edit infra
    
    * edit container port
    
    * edit security groups
    
    * just checking VCS config
    
    * edit security groups
    
    * edit security groups
    
    * docs: add README file (#2)
    
    * add terraform infra files
    
    * edit infra
    
    * edit infra
    
    * edit container port
    
    * edit security groups
    
    * just checking VCS config
    
    * edit security groups
    
    * edit security groups
    
    * add README
    
    * edit README
    
    * edit README
    
    * edit README

---
+++ modules/compute/iam.tf
@@ -0,0 +1,23 @@
+# ---------------------------------------------------------------
+# IAM Role for ECS Task Execution
+# ---------------------------------------------------------------
+
+resource "aws_iam_role" "ecs_task_exec" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_exec_attach" {
+  role       = aws_iam_role.ecs_task_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
